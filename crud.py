5e28f4229697d7db2dfd5bfa82fec6d2c7b721f1
aldairmh <aldairmh02@gmail.com>
2019-10-26 17:13:38 -0500

    reference

---
+++ crud.py
@@ -0,0 +1,17 @@
+def saveVitalData(vitalData):
+    return
+
+def getVitalData():
+    return
+
+def getVitalDataUser():
+    return
+
+def createIncident():
+    return
+
+def removeIncident():
+    return
+
+def getIncidents():
+    return
