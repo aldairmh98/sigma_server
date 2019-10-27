a31b3cf1f379325db1ae45f1cd58d0d57ba91a2d
aldairmh <aldairmh02@gmail.com>
2019-10-27 04:54:08 -0600

    Crud Duro completo Bro

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask import request
 import os
 from pyfcm import FCMNotification
+import crud
 
 
 app = Flask(__name__)
@@ -14,30 +15,30 @@ def index():
 def vitalData():
     if request.method == "POST":
         content = request.json
-        print(content['colonies'])
-        response = jsonify({'Chale': 'Chalecos'})
+        crud.saveVitalData(content)
+        response = jsonify(content)
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
     elif request.method == "GET":
-        data = [
-            {
-                'name': 'Aldair'
-            }
-        ]
-        return jsonify({'datos': data})
+        return jsonify(crud.getVitalData())
 
 @app.route('/vitalData/id/<id>', methods=["GET"])
 def vitalDataId(id):
-    print(request.view_args['id'])
-    return "Hi"
+    id_ = request.view_args['id']
+    response = jsonify(crud.getVitalDataUser(id_))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/incident/<user_id>', methods=["POST"])
 def incidents(user_id):
     # SEARCH FOR USER, SEND NOTIFICATION AND SEND INFORMATION
     #AIzaSyBzu6jeeyGabLpGPj7KutBzR-BmXwsMcrc
     #Expected Input = {USER_ID, LOCATION{latitude, longitude}}
+    id = request.view_args['user_id']
+    crud.createIncident(request.json, id )
     push_service = FCMNotification(api_key="AIzaSyBzu6jeeyGabLpGPj7KutBzR-BmXwsMcrc")
     result = push_service.notify_topic_subscribers(topic_name="covadonga", message_body='hetmm')
+    response = jsonify()
     return "HEY"
 
 if __name__ == '__main__':
